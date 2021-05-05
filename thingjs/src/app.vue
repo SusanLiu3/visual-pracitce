a49aaa34113554087e1c85746a366e50ce56a5a9
liushanshan <2498469106@qq.com>
2021-05-05 15:30:34 +0800

    fixed

--- thingjs/src/app.vue
+++ thingjs/src/app.vue
@@ -1,5 +1,25 @@
 <template>
-    <div id="app">
-        
-    </div>
+  <div id="app">
+   <div id="div3d"></div>
+  </div>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  mounted() {
+     /* eslint-disable-next-line */
+    let app = new THING.App();
+    app.create({
+      type: "campus",
+      url: "https://www.thingjs.com/static/models/storehouse"
+    });
+  }
+}
+</script>
+
+<style>
+</style>

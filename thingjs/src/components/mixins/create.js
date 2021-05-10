13c3b65d04e7181bed9c7342802e1e90193d25ad
liushanshan <2498469106@qq.com>
2021-05-11 07:07:41 +0800

    feat:create

--- thingjs/src/components/mixins/create.js
+++ thingjs/src/components/mixins/create.js
@@ -1,9 +1,9 @@
-export default  {
+export default {
     mounted() {
         /* eslint-disable-next-line */
         let app = new THING.App({
             cotainer: "div3d",
-            url: "https://www.thingjs.com/static/models/storehouse"
+            // url: "https://www.thingjs.com/static/models/storehouse"
         });
         /**
          * create
@@ -19,26 +19,43 @@ export default  {
         // 平面
         app.create({
             type: "Plane",
-            width: 4, // 宽度
-            height: 2, // 高度
+            width: 20, // 宽度
+            height: 20, // 高度
             style: {
                 doubleSide: true
             },
-            position: [0, 9, 0] // 平面中心在世界坐标下的位置ƒ
+            angles: [90, 0, 0],
+            position: [0, 0, 0] // 平面中心在世界坐标下的位置ƒ
         });
         // 圆柱
-        app.create({
+       let cylinder=app.create({
             type: 'Cylinder',
             radius: 2,
-            height: 10,
+            height: 4,
             radiusSegment: 16,
-            position: [10, 2, 0]
+            position: [10, 2, 0],
+            angles: [90, 0, 0]
         })
         // 三角锥;
-        app.create({
+        let tt = app.create({
             type: "Tetrahedron",
             radius: 6,
             position: [2, 0, 2]
         });
+        tt.destroy()
+        app.on('update', function () {
+            // 移动圆柱体(左右移动)
+            cylinder.y = Math.cos(app.elapsedTime * 0.001) * 2;
+        })
+        // 设置最佳观看位置
+        app.camera.fit();
+
+        // 设置主灯光 开启阴影
+        var mainLight = {
+            shadow: true
+        };
+        app.lighting = {
+            mainLight
+        };
     },
 }

d073685e07b4aba6269f7dd45fdf7a673fd43a01
liushanshan <liushanshan@uino.com>
2021-05-13 16:08:01 +0800

    feat:引入three.js

--- thingjs/src/app.vue
+++ thingjs/src/app.vue
@@ -1,24 +1,11 @@
 <template>
     <div id="app">
-<<<<<<< HEAD
-        <object></object>
-=======
         <!-- <campus></campus> -->
         <obj></obj>
->>>>>>> 13c3b65d04e7181bed9c7342802e1e90193d25ad
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-import campus from './components/campus'
-import object from './components/object'
-export default {
-    name: 'App',
-    components: {
-      campus,
-      object
-=======
 // import campus from './components/campus'
 import obj from './components/object'
 export default {
@@ -26,7 +13,6 @@ export default {
     components: {
     //   campus,
       obj
->>>>>>> 13c3b65d04e7181bed9c7342802e1e90193d25ad
     },
     mounted() {
        
--- thingjs/src/components/object.vue
+++ thingjs/src/components/object.vue
@@ -1,48 +1,8 @@
-<<<<<<< HEAD
-<!--
-* @Author: 开发者
-* @Change: 更改者
-* @Date: 年-月-日
-* @Description: app 对象
--->
-<template>
-    <div id="div3d"></div>
-</template>
-
-<script>
-export default {
-    name: '',
-    props: [],
-    data() {
-        return {};
-    },
-    components: {},
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {
-         /* eslint-disable-next-line */
-        let app = new THING.App({
-            cotainer: 'div3d',
-            url: 'https://www.thingjs.com/static/models/storehouse',
-        });
-        app.create({
-            type:'Sphere'
-        })
-    },
-    methods: {},
-    destroyed() {},
-};
-</script>
-
-<style lang="scss" scoped>
-</style>
-=======
 <template>
   <div id="div3d"></div>
 </template>
 <script>
-import createMixin from './mixins/create.js'
+import createMixin from './mixins/create.js';
 export default {
   name: "campus",
   mixins:[createMixin],
@@ -57,4 +17,3 @@ export default {
 //   }
 };
 </script>
->>>>>>> 13c3b65d04e7181bed9c7342802e1e90193d25ad
